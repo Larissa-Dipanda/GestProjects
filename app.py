14dd07bdc572bf0e9e232657e02dbe8ba4c1eefa
Larissa_Dipanda <larissasoppi78@gmail.com>
2026-04-21 03:22:31 +0100

    Version finale 1.2 GestProjects - bilingue FR/EN

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from app import create_app, db
+from app import create_app
+from app.extensions import db
 
 app = create_app()
 
@@ -6,4 +7,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
